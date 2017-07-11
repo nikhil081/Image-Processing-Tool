46e4490abf085dc9c6f5efca6fcc8dccdb5ca3bc
nikhil081 <nikmakkad08@gmail.com>
2017-07-12 01:21:50 +0530

    new commit

---
+++ src/image/Image.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package image;
+
+/**
+ *
+ * @author nikma
+ */
+public class Image {
+
+    static int SCALE_SMOOTH;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new dis().setVisible(true);
+    }
+    
+}
